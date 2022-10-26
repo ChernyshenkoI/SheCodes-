253d73a11f5f6f16909b7765567df4baebc28150
Iryna Chernyshenko <sylkairyna330@gmail.com>
2022-10-26 23:22:19 +0200

    last version before sending

--- 4HW.js
+++ 4HW.js
@@ -12,14 +12,26 @@ function citySearch(event) {
   
   // current time Display
   
+  
   let now = new Date();
   let currentTime = document.querySelector("#time");
   
   let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   let date = now.getDate();
+
   let year = now.getFullYear();
+
+
   let minutes = now.getMinutes();
-  
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+
   let days = [
     "Sunday",
     "Monday",
@@ -50,3 +62,22 @@ function citySearch(event) {
   
   // current time Display END
   
+//   Bonus
+
+function toFahrenheit(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = 66;
+  }
+  
+  function toCelsius(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = 19;
+  }
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", toFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", toCelsius);
