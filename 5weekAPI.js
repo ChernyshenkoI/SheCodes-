ef66818361c7910b5f3056f91d2af59826dcd185
Iryna Chernyshenko <sylkairyna330@gmail.com>
2022-10-26 23:58:43 +0200

    Added API

---
+++ 5weekAPI.js
@@ -0,0 +1,19 @@
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let units = "metric";
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
+
+function showTemperature(response) {
+  console.log(response.data);
+}
+
+
+function showTemperature(response) {
+    let temperature = Math.round(response.data.main.temp);
+    let temperatureElement = document.querySelector("h1");
+    temperatureElement.innerHTML = `It is ${temperature}°C in ${city}`;
+  }
+  
